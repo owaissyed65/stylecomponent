d8e33deed476ec7a7e58d2df838b4985bc10ec25
owaissyed65 <owaissyed65@gmail.com>
2022-12-25 20:15:50 +0500

    styled component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+// import styled from 'styled-components'
+import Styled, { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/GlobalStyles';
 
-function App() {
+// let color ;
+const App = () => {
+  const Button = Styled.button`
+  color:${({ bg }) => bg.col};
+  background-color:${({ bg }) => bg.bg};
+  outline:none;
+  // border:none;
+  cursor:pointer;
+  display:block;
+  width:${({ theme }) => theme.width};
+  height:10rem;
+&:hover{
+  background-color:${() => color.bg === 'red' ? 'blue' : 'red'};
+  color:white;
+}
+  `;
+  const theme = {
+    width: '12rem',
+    bgColorBody: 'blue'
+  }
+  const [color, setColor] = useState({ bg: 'red', col: 'blue' });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <div style={{ width: '10rem', backgroundColor: 'blue', margin: '0 auto' }}>
+        <Button bg={color} onClick={() => { color.bg === 'red' ? setColor({ ...color, bg: 'blue', col: 'red' }) : setColor({ ...color, bg: 'red', col: 'blue' }); }}>Subscribe to me</Button>
+      </div>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
